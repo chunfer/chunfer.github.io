565b3173dbb366bd7cfedb107c207ec42003bc05
Fernando Juarez <juanfmontufarjuarez@gmail.com>
2020-10-23 17:21:58 -0600

    Modificando ventana de navegacion

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("mySidenav").style.width = "15em";
+  document.getElementById("main").style.marginLeft = "16em";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("main").style.marginLeft= "1em";
   document.body.style.backgroundColor = "white";
 }
