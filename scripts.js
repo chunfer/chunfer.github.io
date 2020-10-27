b0eac739239994c0da465eb8f3bb39f8da220d90
Fernando Juarez <juanfmontufarjuarez@gmail.com>
2020-10-27 01:07:51 -0600

    Modificando a mi sitio web de presentacion personal

--- scripts.js
+++ scripts.js
@@ -1,11 +1,24 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "15em";
-  document.getElementById("main").style.marginLeft = "16em";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+function NavManager() {
+  var arrayinfo = document.getElementsByClassName("info");
+
+  if (document.getElementById("closebtn").innerHTML == ""){
+    document.getElementById("mySidenav").style.width = "15em";
+    document.getElementById("main").style.marginLeft = "16em";
+    document.body.style.opacity = 0.7;
+    document.getElementById("closebtn").className = "	fas fa-angle-left"; 
+    document.getElementById("closebtn").innerHTML = " ";
+    for (let index = 0; index < arrayinfo.length; index++) {
+      arrayinfo[index].style.display = 'block';
+    }
+  } else {
+    document.getElementById("mySidenav").style.width = "45px";
+    document.getElementById("main").style.marginLeft= "63px";
+    document.body.style.opacity = 1;
+    document.getElementById("closebtn").className = "	fas fa-angle-right";
+    document.getElementById("closebtn").innerHTML = "";
+    for (let index = 0; index < arrayinfo.length; index++) {
+      arrayinfo[index].style.display = 'none';
+    }
+  }
 }
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "1em";
-  document.body.style.backgroundColor = "white";
-}
