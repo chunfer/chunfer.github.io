c2093317d4f283966a08e38b576d652310ef15e4
Fernando Montufar <201403684@ingenieria.usac.edu.gt>
2020-10-30 16:25:21 -0600

    Version casi final con responsive design

--- scripts.js
+++ scripts.js
@@ -1,3 +1,48 @@
+const s_smart = window.matchMedia('screen and (max-width: 320px)')
+const l_smart = window.matchMedia('screen and (max-width: 480px)')
+const ipad = window.matchMedia('screen and (max-width: 767px)')
+
+const work_link = document.querySelector('.work-link');
+const exp_link = document.querySelector('.exp-link');
+const port_link = document.querySelector('.port-link');
+
+const burgerButton = document.querySelector('#hamburger');
+
+if (s_smart.matches) {
+  burgerButton.addEventListener('click', hideShow3)
+
+} else {
+  burgerButton.removeEventListener('click', hideShow3)
+
+  if (l_smart.matches){
+    burgerButton.addEventListener('click', hideShow2)
+  }else{
+    burgerButton.removeEventListener('click', hideShow2)
+  
+    if (ipad.matches) {
+      burgerButton.addEventListener('click', hideShow)  
+    } else {
+      burgerButton.removeEventListener('click', hideShow)
+    }
+  }
+}
+
+
+function hideShow() {
+  work_link.classList.toggle("is-active");
+}
+
+function hideShow2() {
+  work_link.classList.toggle("is-active");
+  exp_link.classList.toggle("is-active");
+}
+
+function hideShow3() {
+  work_link.classList.toggle("is-active");
+  exp_link.classList.toggle("is-active");
+  port_link.classList.toggle("is-active");
+}
+
 function NavManager() {
   var arrayinfo = document.getElementsByClassName("info");
 
@@ -25,7 +70,6 @@ function NavManager() {
       arrayinfo[index].style.display = 'none';
     }
     document.getElementById("main-headerContent-h1").style.fontSize = "2.8vw";
-
   }
 }
 
